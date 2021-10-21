276ece9b77a28d1c177320e0f4c6054b585a4315
itsquicks <38190793+itsquicks@users.noreply.github.com>
2021-10-22 01:17:28 +0300

    Delete src directory

--- src/Drivers/CPU.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../Typedefs.h"
-#include "../IO.h"
-#include "../Memory/PageFrameAllocator.h"
-
-void CPUID(uint32 code, uint32& eax, uint32& ebx, uint32& ecx, uint32& edx);
-char* CPU_GetName();
-uint64 CPU_GetSpeed();
-uint64 RDTSC();
--- src/kmath.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Typedefs.h"
-
-uint64 max(uint64 a, uint64 b);
-uint64 min(uint64 a, uint64 b);
-int abs(int n);
