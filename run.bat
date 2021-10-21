3ac1045189f0fc32f2c2bbce55f9d3d3a0af14e6
itsquicks <38190793+itsquicks@users.noreply.github.com>
2021-10-22 01:15:02 +0300

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 cls
 @pushd %~dp0
 
-qemu-system-x86_64 -drive format=raw,file=bin\CustomOS.img -m 256M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file=..\OVMFbin\OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=..\OVMFbin\OVMF_VARS-pure-efi.fd -net none -hdb C:\Users\Fabian\Desktop\ProjectOS\hd.iso
+qemu-system-x86_64 -drive format=raw,file=bin\CustomOS.img -m 256M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file=..\OVMFbin\OVMF_CODE-pure-efi.fd,readonly=on -drive if=pflash,format=raw,unit=1,file=..\OVMFbin\OVMF_VARS-pure-efi.fd -net none -hdb C:\Users\Fabian\Desktop\ext2drive.iso
 
 @popd
-
